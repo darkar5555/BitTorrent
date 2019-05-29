c54da7ade6ee5da756b2ce51c04b4aa14a8a7733
eyvind1 <eyvind1@gitlab.com>
2019-05-29 15:26:32 -0500

    basic function

---
+++ peer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string.h>
+#include <fstream>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <map>
+#include "functions.h"
+
+using namespace std;
+
+int main() {
+   
+
+   return 0;
+}
